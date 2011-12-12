86b50a209f7b584512931d6a23520345dc8c3127
Paul M. Jones <pmjones88@gmail.com>
2011-12-12 11:53:42 -0600

    use subdirs

--- src.php
+++ src.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/src/Cookies.php';
-require_once __DIR__ . '/src/Exception.php';
-require_once __DIR__ . '/src/Exception/HeadersSent.php';
-require_once __DIR__ . '/src/Headers.php';
-require_once __DIR__ . '/src/Response.php';
+require_once __DIR__ . '/src/Aura/Http/Cookies.php';
+require_once __DIR__ . '/src/Aura/Http/Exception.php';
+require_once __DIR__ . '/src/Aura/Http/Exception/HeadersSent.php';
+require_once __DIR__ . '/src/Aura/Http/Headers.php';
+require_once __DIR__ . '/src/Aura/Http/Response.php';

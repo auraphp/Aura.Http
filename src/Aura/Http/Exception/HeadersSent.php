7e5e80950cd1631639c367b903a629c7a4a0e5cf
Paul M. Jones <pmjones88@gmail.com>
2011-12-27 18:35:30 -0600

    update comments

--- src/Aura/Http/Exception/HeadersSent.php
+++ src/Aura/Http/Exception/HeadersSent.php
@@ -17,6 +17,15 @@ namespace Aura\Http\Exception;
  */
 class HeadersSent extends \Aura\Http\Exception
 {
+    /**
+     * 
+     * Constructor.
+     * 
+     * @param string $file The file from which headers were first sent.
+     * 
+     * @param int $line The line number in that file where headers were sent.
+     * 
+     */
     public function __construct($file, $line)
     {
         $message = "Headers have already been sent from '{$file}' at line {$line}.";

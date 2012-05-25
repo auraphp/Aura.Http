9d566ade246907d06711e68bf785249a703c46ff
Paul M. Jones <pmjones88@gmail.com>
2012-05-25 11:39:08 -0500

    use the right class

--- src/Aura/Http/Request/Factory.php
+++ src/Aura/Http/Request/Factory.php
@@ -6,17 +6,13 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  */
-namespace Aura\Http\Factory;
+namespace Aura\Http\Request;
 
-use Aura\Http\Header\Collection as Headers;
 use Aura\Http\Cookie\Collection as Cookies;
-use Aura\Http\Request as HttpRequest;
-use Aura\Http\Request\Response as RequestResponse;
-use Aura\Http\Cookie\Jar as CookieJar;
-use Aura\Http\Transport\Multipart;
-use Aura\Http\Request\ResponseBuilder;
-use Aura\Http\Request\Adapter\Curl;
-use Aura\Http\Request\Adapter\Stream;
+use Aura\Http\Cookie\Factory as CookieFactory;
+use Aura\Http\Header\Collection as Headers;
+use Aura\Http\Header\Factory as HeaderFactory;
+use Aura\Http\Request as Request;
 
 /**
  * 
@@ -25,14 +21,11 @@ use Aura\Http\Request\Adapter\Stream;
  * @package Aura.Http
  * 
  */
-class Request
+class Factory
 {
     /**
      *
      * Convenience method for creating a Request object. 
-     * 
-     * @param string $adapter Use this adapter. Defaults to `auto` if Curl is 
-     * installed the Curl adapter is used else the Stream adapter is used.
      * 
      * @param array $options Adapter specific options and defaults. Currently 
      * only used by Curl.
@@ -40,27 +33,10 @@ class Request
      * @return Aura\Http\Request
      *
      */
-    public function newInstance($adapter = 'auto', array $options = [])
+    public function newInstance(array $options = [])
     {
-        $headers          = new Headers(new Header);
-        $cookiefactory    = new Cookie;
-        $cookies          = new Cookies($cookiefactory);
-        $response         = new RequestResponse($headers, $cookies);
-        $response_builder = new ResponseBuilder($response, new ResponseStack);
-
-        if ('curl' == $adapter ||
-            ('auto' == $adapter && extension_loaded('curl'))) {
-            
-            $adapter   = new Curl($response_builder, $options);
-        } else {
-            $cookiejar = new CookieJar($cookiefactory);
-            $adapter   = new Stream(
-                                $response_builder, 
-                                new Multipart, 
-                                $cookiejar
-                            );
-        }
-
-        return new HttpRequest($adapter, $headers, $cookies);
+        $headers = new Headers(new HeaderFactory);
+        $cookies = new Cookies(new CookieFactory);
+        return new Request($headers, $cookies, $options);
     }
 }

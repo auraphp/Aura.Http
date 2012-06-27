0822b647fb3d1e9c0c50ea554f5da7593fe14115
Paul M. Jones <pmjones88@gmail.com>
2012-06-26 19:35:47 -0500

    correct construction of new adapters

--- scripts/instance.php
+++ scripts/instance.php
@@ -1,32 +1,28 @@
 <?php
 namespace Aura\Http;
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src.php';
-
 if (extension_loaded('curl')) {
-    // use curl adapter for transport
     return new Manager(
         new Message\Factory,
         new Transport(
             new PhpFunc,
             new Transport\Options,
+            // use curl adapter for transport
             new Adapter\Curl(
-                new Message\Response\StackBuilder(
-                    new Message\Factory
-                )
+                new Message\Response\StackBuilder(new Message\Factory)
             )
         )
     );
 } else {
-    // use stream adapter for transport
     return new Manager(
         new Message\Factory,
         new Transport(
             new PhpFunc,
             new Transport\Options,
+            // use stream adapter for transport
             new Adapter\Stream(
-                new Message\Response\StackBuilder(
-                    new Message\Factory
-                )
+                new Message\Response\StackBuilder(new Message\Factory),
+                new Multipart\FormData(new Multipart\PartFactory)
             )
         )
     );

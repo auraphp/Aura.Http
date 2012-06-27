1d401007b0437e81e1d5021034249f49c9e55fd6
Paul M. Jones <pmjones88@gmail.com>
2012-06-27 08:08:52 -0500

    finish first pass at new wiring configs

--- config/default.php
+++ config/default.php
@@ -4,30 +4,44 @@ $loader->add('Aura\Http\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 /**
  * Constructor params.
  */
-$di->config['Aura\Http\Adapter\Curl'] = [
+
+$di->params['Aura\Http\Adapter\Curl'] = [
     'stack_builder' => $di->lazyNew('Aura\Http\Message\Response\StackBuilder'),
 ];
 
-$di->config['Aura\Http\Adapter\Stream'] = [
+$di->params['Aura\Http\Adapter\Stream'] = [
     'stack_builder'      => $di->lazyNew('Aura\Http\Message\Response\StackBuilder'),
     'form_data'          => $di->lazyNew('Aura\Http\Multipart\FormData'),
     'cookie_jar_factory' => $di->lazyNew('Aura\Http\Cookie\JarFactory'),
 ];
 
-$di->config['Aura\Http\Manager'] = [
-    'message_factory' => $di->lazyNew('Aura\Http\Message\Factory',
-    'transport'       => $di->lazyNew('Aura\Http\Transport'),
+$di->params['Aura\Http\Cookie\Collection'] = [
+    'factory' => $di->lazyNew('Aura\Http\Cookie\Factory'),
 ];
 
-$di->config['Aura\Http\Message\Response\StackBuilder'] = [
+$di->params['Aura\Http\Header\Collection'] = [
+    'factory' => $di->lazyNew('Aura\Http\Header\Factory'),
+];
+
+$di->params['Aura\Http\Manager'] = [
+    'message_factory' => $di->lazyNew('Aura\Http\Message\Factory'),
+    'transport'       => $di->lazyGet('http_transport'),
+];
+
+$di->params['Aura\Http\Message'] = [
+    'headers' => $di->lazyNew('Aura\Http\Header\Collection'),
+    'cookies' => $di->lazyNew('Aura\Http\Cookie\Collection'),
+];
+
+$di->params['Aura\Http\Message\Response\StackBuilder'] = [
     'message_factory' => $di->lazyNew('Aura\Http\Message\Factory'),
 ];
 
-$di->config['Aura\Http\Multipart\FormData'] = [
+$di->params['Aura\Http\Multipart\FormData'] = [
     'part_factory' => $di->lazyNew('Aura\Http\Multipart\PartFactory'),
 ];
 
-$di->config['Aura\Http\Transport'] = [
+$di->params['Aura\Http\Transport'] = [
     'phpfunc' => $di->lazyNew('Aura\Http\PhpFunc'),
     'options' => $di->lazyNew('Aura\Http\Transport\Options'),
     'adapter' => extension_loaded('curl')
@@ -38,6 +52,11 @@ $di->config['Aura\Http\Transport'] = [
 /**
  * Dependency services.
  */
-$di->service('http_manager', function () use ($di) {
+
+$di->set('http_transport', function() use ($di) {
+    return $di->newInstance('Aura\Http\Transport');
+});
+
+$di->set('http_manager', function () use ($di) {
     return $di->newInstance('Aura\Http\Manager');
 });

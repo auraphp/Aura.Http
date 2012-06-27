c620783eacf25c5422915104f6eaa6ea1232a440
Paul M. Jones <pmjones88@gmail.com>
2012-06-27 09:38:38 -0500

    streamline adapter picking

--- scripts/instance.php
+++ scripts/instance.php
@@ -1,30 +1,21 @@
 <?php
 namespace Aura\Http;
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src.php';
-if (extension_loaded('curl')) {
-    return new Manager(
-        new Message\Factory,
-        new Transport(
-            new PhpFunc,
-            new Transport\Options,
-            // use curl adapter for transport
-            new Adapter\Curl(
-                new Message\Response\StackBuilder(new Message\Factory)
-            )
-        )
-    );
-} else {
-    return new Manager(
-        new Message\Factory,
-        new Transport(
-            new PhpFunc,
-            new Transport\Options,
-            // use stream adapter for transport
-            new Adapter\Stream(
-                new Message\Response\StackBuilder(new Message\Factory),
-                new Multipart\FormData(new Multipart\PartFactory),
-                new Cookie\JarFactory,
-            )
-        )
-    );
-}
+return new Manager(
+    new Message\Factory,
+    new Transport(
+        new PhpFunc,
+        new Transport\Options,
+        // if curl is loaded, use the curl adapter,
+        // otherwise use the stream adapter
+        extension_loaded('curl')
+            ?   new Adapter\Curl(
+                    new Message\Response\StackBuilder(new Message\Factory)
+                )
+            :   new Adapter\Stream(
+                    new Message\Response\StackBuilder(new Message\Factory),
+                    new Multipart\FormData(new Multipart\PartFactory),
+                    new Cookie\JarFactory
+                )
+    )
+);

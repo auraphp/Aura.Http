083543cf72c8cf8a5475403d3052633ce4bede7c
Paul M. Jones <pmjones88@gmail.com>
2012-06-26 23:36:38 -0500

    add default wiring

--- config/default.php
+++ config/default.php
@@ -1,55 +1,43 @@
 <?php
-
 $loader->add('Aura\Http\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
 /**
  * Constructor params.
  */
-$di->params['Aura\Http\Cookie\Collection'] = [
-    'factory' => $di->lazyNew('Aura\Http\Cookie\Factory'),
-];
-$di->params['Aura\Http\Header\Collection'] = [
-    'factory' => $di->lazyNew('Aura\Http\Header\Factory'),
-
-];
-$di->params['Aura\Http\Response'] = [
-    'headers' => $di->lazyNew('Aura\Http\Header\Collection'),
-    'cookies' => $di->lazyNew('Aura\Http\Cookie\Collection'),
-];
-$di->params['Aura\Http\Request\Response'] = [
-    'headers' => $di->lazyNew('Aura\Http\Header\Collection'),
-    'cookies' => $di->lazyNew('Aura\Http\Cookie\Collection'),
-];
-$di->params['Aura\Http\Request\ResponseBuilder'] = [
-    'response' => $di->lazyNew('Aura\Http\RequestResponse'),
-    'factory'  => $di->lazyNew('Aura\Http\Request\ResponseStackFactory'),
-];
-$di->params['Aura\Http\Request\Adapter\Curl'] = [
-    'builder' => $di->lazyNew('Aura\Http\Request\ResponseBuilder'),
-    'options' => [],
-];
-$di->params['Aura\Http\Request\Adapter\Stream'] = [
-    'builder'   => $di->lazyNew('Aura\Http\Request\ResponseBuilder'),
-    'multipart' => $di->lazyNew('Aura\Http\Transport\Multipart'),
+$di->config['Aura\Http\Adapter\Curl'] = [
+    'stack_builder' => $di->lazyNew('Aura\Http\Message\Response\StackBuilder'),
 ];
 
-$adapter = extension_loaded('curl') ? 'Aura\Http\Request\Adapter\Curl' : 'Aura\Http\Request\Adapter\Stream';
-
-$di->params['Aura\Http\Request'] = [
-    'adapter' => $di->lazyNew($adapter),
-    'headers' => $di->lazyNew('Aura\Http\Header\Collection'),
-    'cookies' => $di->lazyNew('Aura\Http\Cookie\Collection'),
-    'options' => [],
+$di->config['Aura\Http\Adapter\Stream'] = [
+    'stack_builder'      => $di->lazyNew('Aura\Http\Message\Response\StackBuilder'),
+    'form_data'          => $di->lazyNew('Aura\Http\Multipart\FormData'),
+    'cookie_jar_factory' => $di->lazyNew('Aura\Http\Cookie\JarFactory'),
 ];
 
+$di->config['Aura\Http\Manager'] = [
+    'message_factory' => $di->lazyNew('Aura\Http\Message\Factory',
+    'transport'       => $di->lazyNew('Aura\Http\Transport'),
+];
+
+$di->config['Aura\Http\Message\Response\StackBuilder'] = [
+    'message_factory' => $di->lazyNew('Aura\Http\Message\Factory'),
+];
+
+$di->config['Aura\Http\Multipart\FormData'] = [
+    'part_factory' => $di->lazyNew('Aura\Http\Multipart\PartFactory'),
+];
+
+$di->config['Aura\Http\Transport'] = [
+    'phpfunc' => $di->lazyNew('Aura\Http\PhpFunc'),
+    'options' => $di->lazyNew('Aura\Http\Transport\Options'),
+    'adapter' => extension_loaded('curl')
+               ? $di->lazyNew('Aura\Http\Adapter\Curl')
+               : $di->lazyNew('Aura\Http\Adapter\Stream'),
+];
 
 /**
  * Dependency services.
  */
-$di->set('http_response', function() use ($di) {
-    return $di->newInstance('Aura\Http\Response');
-});
-
-$di->set('http_request', function() use ($di) {
-    return $di->newInstance('Aura\Http\Request');
+$di->service('http_manager', function () use ($di) {
+    return $di->newInstance('Aura\Http\Manager');
 });

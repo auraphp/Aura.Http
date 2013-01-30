6adb31b0932df0a3bc660d3c7295762009e9c2c1
Paul M. Jones <pmjones88@gmail.com>
2013-01-30 01:09:25 -0600

    add wiring test

---
+++ tests/WiringTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Aura\Http;
+
+use Aura\Framework\Test\WiringAssertionsTrait;
+
+class WiringTest extends \PHPUnit_Framework_TestCase
+{
+    use WiringAssertionsTrait;
+
+    protected function setUp()
+    {
+        $this->loadDi();
+    }
+
+    public function testServices()
+    {
+        $this->assertGet('http_transport', 'Aura\Http\Transport');
+        $this->assertGet('http_manager', 'Aura\Http\Manager');
+    }
+
+    public function testInstances()
+    {
+        $this->assertNewInstance('Aura\Http\Adapter\Curl');
+        $this->assertNewInstance('Aura\Http\Adapter\Stream');
+        $this->assertNewInstance('Aura\Http\Cookie\Collection');
+        $this->assertNewInstance('Aura\Http\Header\Collection');
+        $this->assertNewInstance('Aura\Http\Manager');
+        $this->assertNewInstance('Aura\Http\Message');
+        $this->assertNewInstance('Aura\Http\Message\Response\StackBuilder');
+        $this->assertNewInstance('Aura\Http\Multipart\FormData');
+        $this->assertNewInstance('Aura\Http\Transport');
+    }
+}
